2259c48d97c47cffe3894d320fd67f2db7eed2e9
Joseph Greenwell <joeygreenwell@ymail.com>
2021-05-04 14:44:02 -0500

    adding association

--- models/index.js
+++ models/index.js
@@ -1,29 +1,14 @@
 const User = require('./User');
 const Recipe = require('./Recipe');
-const UserRecipe = require('./UserRecipe');
 
-
-UserRecipe.belongsTo(User, {
-    foreignKey: 'user_id',
-  });
-  
-// Recipe.hasMany(User, {
-//     foreignKey: 'id',
-//   });
-
-UserRecipe.belongsTo(Recipe, {
-    foreignKey: 'recipe_id',
+User.hasMany(Recipe,{
+  foreignKey:'user_id'
 });
 
-// User.hasMany(Recipe, {
-//     through: Recipe,
-//     foreignKey: 'id',
-// });
 
-// Recipe.belongsToMany(User, {
-//     through: User,
-//     foreignKey: 'id',
-// });
+Recipe.belongsTo(User,{
+  foreignKey:'user_id'
+});
 
 
-module.exports = { User, Recipe, UserRecipe };
+module.exports = { User, Recipe,};
