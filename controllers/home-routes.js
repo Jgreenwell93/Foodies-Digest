a69c7a9ea438a43c147e72b480a7f555dfecccfb
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-05-04 20:20:08 -0500

    committing changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,13 @@
 const { Recipe } = require('../models');
 const { User } = require('../models')
 
+
 const router = require('express').Router();
 // const withAuth = require('../../utils/auth');
 
 var apikey = '481279d87a314d4d94b73f8882f542e7'
 var apiUrl = 'https://api.spoonacular.com/recipes/random?' + 'apiKey=' + apikey + '&number=3';
+const fetch = require('node-fetch');
 
 // get route for homepage
 // router.get('/', async (req, res) => {
@@ -28,24 +30,33 @@ var apiUrl = 'https://api.spoonacular.com/recipes/random?' + 'apiKey=' + apikey 
 //     }
 //   });
 
-  router.get('/', async (req, res) => {
-    try {
-      const recipeData = await fetch(apiUrl).then(function (response) {
-          if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-              });              
-          }
-      })
+router.get('/', async (req, res) => {
+  try {
+    const recipeData = await fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+              // console.log(data);
+            const recipeArray = data.recipes.map( (recipe) => {
+              return {
+                id: recipe.id,
+                title: recipe.title,
+                instructions: recipe.instructions,
+                image: recipe.image,
+              }
+            });  
+            console.log(recipeArray);
 
-      res.render('homepage', {
-        recipeData
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+            res.render('homepage', {
+            recipeArray
+          });            
+          });
+      }    
+  })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
   router.get('/login', (req, res) => {
     // if (req.session.loggedIn) {
