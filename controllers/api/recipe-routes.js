4a113e67881829d2a65272f4b269c65b9996f047
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-05-06 10:17:18 -0500

    styling and logo

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -18,57 +18,57 @@ router.post('/', withAuth, async (req, res) => {
 
 // route tied to Fetch (userRecipes/favorites) on front end for get all title/image:
 // READ recipe GET ALL ROUTE to return db for logged in user (image, title, id?)
-router.get('/:favorites', withAuth, async (req, res) => {
-    try {
-        const allRecipeData = await Recipe.findAll({
-            include: [
-                {
-                    model: Recipe,
-                    attributes: ['image', 'title', 'id'],
-                },
-            ],
-        });
-        const allRecipe = allRecipeData.map((allRecipe) =>
-            allRecipe.get({ plain: true })
-        );
-        res.render('favorites', {
-            allRecipe,
-            loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
+// router.get('/:favorites', withAuth, async (req, res) => {
+//     try {
+//         const allRecipeData = await Recipe.findAll({
+//             include: [
+//                 {
+//                     model: Recipe,
+//                     attributes: ['image', 'title', 'id'],
+//                 },
+//             ],
+//         });
+//         const allRecipe = allRecipeData.map((allRecipe) =>
+//             allRecipe.get({ plain: true })
+//         );
+//         res.render('favorites', {
+//             allRecipe,
+//             loggedIn: req.session.loggedIn,
+//         });
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// });
 
 
 // route tied to Fetch (userRecipes/favorites) on front end for get one/return full recipe:
 // READ recipe GET ONE ROUTE to return db for logged in user (full recipe)
-router.get('/:title', withAuth, async (req, res) => {
-    try {
-        const recipeData = await Recipe.findOne({
-            where: { title: req.params.title },
-            include: [{
-                model: Recipe
-            },
-            ],
-        });
-        if (!recipeData) {
-            res.status(404).json({ message: 'No Recipe Found!' });
-            return;
-        }
-        const fullRecipe = recipeData.map((fullRecipe) =>
-            fullRecipe.get({ plain: true })
-        );
-        res.render('fullrecipe', {
-            fullRecipe,
-            loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
+// router.get('/:title', withAuth, async (req, res) => {
+//     try {
+//         const recipeData = await Recipe.findOne({
+//             where: { title: req.params.title },
+//             include: [{
+//                 model: Recipe
+//             },
+//             ],
+//         });
+//         if (!recipeData) {
+//             res.status(404).json({ message: 'No Recipe Found!' });
+//             return;
+//         }
+//         const fullRecipe = recipeData.map((fullRecipe) =>
+//             fullRecipe.get({ plain: true })
+//         );
+//         res.render('fullrecipe', {
+//             fullRecipe,
+//             loggedIn: req.session.loggedIn,
+//         });
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// });
 
 // route tied to PUT on front end to remove from favorites:
 // route to DELETE/DESTROY saved receipes (if time)
